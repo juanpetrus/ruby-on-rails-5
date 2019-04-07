6eb71fe2742d4bfd849767a64fd7fd43c3ea4496
Juan Petrus <juan.petruss@gmail.com>
2019-04-07 14:28:42 -0400

    Create model contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static_pages#index"
 
-  get 'contact', to: 'static_pages#contact'
+  get 'contac', to: 'static_pages#contact'
   get 'about', to: 'static_pages#about'
 end
