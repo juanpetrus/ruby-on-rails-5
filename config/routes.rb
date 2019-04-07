eb540acd91ab25fe3a7adbc792dd1ddad4d47ac6
Juan Petrus <juan.petruss@gmail.com>
2019-04-07 18:09:35 -0400

    Valid controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static_pages#index"
 
   get 'contac', to: 'static_pages#contact'
   get 'about', to: 'static_pages#about'
+
+  resources :contacts
+  resources :users, only: [:new, :create]
 end
